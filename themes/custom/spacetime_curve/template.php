fc1e249ff7b3253e0565c2f5b817fc676d074c97
Mr Snow <mr.snow@houseoflaudanum.com>
2012-04-18 14:42:08 +1000

    #23 Better header and masthead, new logo.

--- themes/custom/spacetime_curve/template.php
+++ themes/custom/spacetime_curve/template.php
@@ -1,4 +1,5 @@
 <?php
-
-
+  function spacetime_curve_preprocess_html(&$variables) {
+    drupal_add_css('http://fonts.googleapis.com/css?family=Ubuntu:400,500,400italic,500italic', array('type' => 'external'));
+  } 
 ?>
