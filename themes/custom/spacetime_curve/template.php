3a55cecbe3da31c7f8b473424a64488e65ee8400
Mr Snow <mr.snow@houseoflaudanum.com>
2012-04-27 14:15:35 +1000

    #67 Begin IE8 compat.

--- themes/custom/spacetime_curve/template.php
+++ themes/custom/spacetime_curve/template.php
@@ -3,6 +3,22 @@
     drupal_add_css('http://fonts.googleapis.com/css?family=Ubuntu:400,500,400italic,500italic', array('type' => 'external'));
     if ( isset($_REQUEST['render']) )
       $variables['theme_hook_suggestion'] = 'html__render';
+
+// Add conditional stylesheets for IE.
+    drupal_add_css(
+      drupal_get_path('theme', 'spacetime_curve') . '/libs/css/ie.css',
+      array(
+        'group' => CSS_THEME,
+        'browsers' => array(
+  #        'IE' => 'lte IE 7',
+          'IE' => 'IE',
+          '!IE' => FALSE,
+        ),
+        'weight' => 999,
+        'every_page' => TRUE,
+      )
+    );
+
   } 
   
   /*
@@ -13,20 +29,6 @@
     if ( isset($_REQUEST['render']) )
       $variables['theme_hook_suggestion'] = 'page__render';
   
-// Add conditional stylesheets for IE.
-  drupal_add_css(
-    drupal_get_path('theme', 'spacetime_curve') . '/libs/css/ie.css',
-    array(
-      'group' => CSS_THEME,
-      'browsers' => array(
-#        'IE' => 'lte IE 7',
-        'IE' => 'IE',
-        '!IE' => FALSE,
-      ),
-      'weight' => 999,
-      'every_page' => TRUE,
-    )
-  );
 
   }
 ?>
